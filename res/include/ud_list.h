b641e8dfc8d3a4a53705106bf4ddd9258249c590
sbedene <sylvainbedene@gmail.com>
2019-10-22 13:03:16 +0200

    remove list push las

--- res/include/ud_list.h
+++ res/include/ud_list.h
@@ -32,55 +32,46 @@
         _list; \
     })
 
-# define ud_list_mpush_ctr(last, ctype, _list) \
+# define ud_list_mpush_ctr(ctype, _list) \
     ({ \
         ctype *_curr = _list; \
-        if (!last) \
-            while (_curr->next) \
-                _curr = _curr->next; \
+        while (_curr->next) \
+            _curr = _curr->next; \
         _curr->next = ud_list_init_ctr(sizeof(*_list)); \
         _curr->next; \
     })
 
-# define ud_list_push_ctr(last, ctype, _list, declaration) \
+# define ud_list_push_ctr(ctype, _list, declaration) \
     ({ \
         ctype *_curr = _list; \
-        if (!last) \
-            while (_curr->next) \
-                _curr = _curr->next; \
+        while (_curr->next) \
+            _curr = _curr->next; \
         _curr->next = ud_list_init_ctr(sizeof(*_list)); \
         _curr->next->declaration; \
         _curr->next; \
     })
 
-# define ud_list_fpush_ctr(last, ctype, _list, fp, ...) \
+# define ud_list_fpush_ctr(ctype, _list, fp, ...) \
     ({ \
         ctype *_curr = _list; \
-        if (!last) \
-            while (_curr->next) \
-                _curr = _curr->next; \
+        while (_curr->next) \
+            _curr = _curr->next; \
         _curr->next = ud_list_init_ctr(sizeof(*_list)); \
         fp(_curr->next, __VA_ARGS__); \
         _curr->next; \
     })
 
-# define ud_list_push(ctype, _list, declaration)        ud_list_push_ctr(0, ctype, _list, declaration)
-# define ud_list_push_last(ctype, _list, declaration)   ud_list_push_ctr(1, ctype, _list, declaration)
-# define ud_list_mpush(ctype, _list)                    ud_list_mpush_ctr(0, ctype, _list)
-# define ud_list_mpush_last(ctype, _list)               ud_list_mpush_ctr(1, ctype, _list)
-# define ud_list_fpush(ctype, _list, fp, ...)           ud_list_fpush_ctr(0, ctype, _list, fp, __VA_ARGS__)
-# define ud_list_fpush_last(ctype, _list, fp, ...)      ud_list_fpush_ctr(1, ctype, _list, fp, __VA_ARGS__)
+# define ud_list_push(ctype, _list, declaration)        ud_list_push_ctr(ctype, _list, declaration)
+# define ud_list_mpush(ctype, _list)                    ud_list_mpush_ctr(ctype, _list)
+# define ud_list_fpush(ctype, _list, fp, ...)           ud_list_fpush_ctr(ctype, _list, fp, __VA_ARGS__)
 
 // macros for using ud_lst instead of ud_list
 # define ud_lst_init(ctype, declaration)                ud_list_init(ctype, declaration)
 # define ud_lst_finit(ctype, fp, ...)                   ud_list_init(ctype, fp, __VA_ARGS__)
 # define ud_lst_minit(ctype)                            ud_list_minit(ctype)
-# define ud_lst_push(ctype, _list, declaration)         ud_list_push_ctr(0, ctype, _list, declaration)
-# define ud_lst_push_last(ctype, _list, declaration)    ud_list_push_ctr(1, ctype, _list, declaration)
-# define ud_lst_mpush(ctype, _list)                     ud_list_mpush_ctr(0, ctype, _list)
-# define ud_lst_mpush_last(ctype, _list)                ud_list_mpush_ctr(1, ctype, _list)
-# define ud_lst_fpush(ctype, _list, fp, ...)            ud_list_fpush_ctr(0, ctype, _list, fp, __VA_ARGS__)
-# define ud_lst_fpush_last(ctype, _list, fp, ...)       ud_list_fpush_ctr(1, ctype, _list, fp, __VA_ARGS__)
+# define ud_lst_push(ctype, _list, declaration)         ud_list_push_ctr(ctype, _list, declaration)
+# define ud_lst_mpush(ctype, _list)                     ud_list_mpush_ctr(ctype, _list)
+# define ud_lst_fpush(ctype, _list, fp, ...)            ud_list_fpush_ctr(ctype, _list, fp, __VA_ARGS__)
 # define ud_lst_free(ctype, _list)                      ud_list_free(_list)
 
 
