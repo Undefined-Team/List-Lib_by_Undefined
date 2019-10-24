b81c8bdcc02b6b4cb43006fb7c855eb76f4f3242
sbedene <sylvainbedene@gmail.com>
2019-10-24 11:11:11 +0200

    renamed UD_UT_PROT_MALLOC to ud_ut_prot_malloc

--- res/src/init.c
+++ res/src/init.c
@@ -4,7 +4,7 @@ void        *ud_list_init_ctr(size_t len)
 {
     void    *elem = NULL;
 
-    UD_UT_PROT_MALLOC(elem = ud_ut_malloc(len));
+    ud_ut_prot_malloc(elem = ud_ut_malloc(len));
     ud_mem_set(elem, 0, len);
     return elem;
 }
